376c8d24a06d337bb0e6d0a721516c441b1bebe6
kfedus6 <feduskolya@gmail.com>
2021-08-17 18:17:17 +0300

    Sample code

--- js/index.js
+++ js/index.js
@@ -515,3 +515,35 @@ let user = {
 
 alert(count(user));*/
 
+//Напишите функцию printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to.
+//Сделайте два варианта решения.
+//Используя setInterval.
+//Используя рекурсивный setTimeout.
+
+/*function printNumbers(from, to) {
+   let current = from;
+
+   let timerId = setInterval(function () {
+      alert(current);
+      if (current == to) {
+         clearInterval(timerId);
+      }
+      current++;
+   }, 1000);
+}
+
+printNumbers(5, 10);*/
+
+/*function printNumbers(from, to) {
+   let current = from;
+
+   setTimeout(function go() {
+      alert(current);
+      if (current < to) {
+         setTimeout(go, 1000);
+      }
+      current++;
+   }, 1000)
+}
+
+printNumbers(5, 10);*/
